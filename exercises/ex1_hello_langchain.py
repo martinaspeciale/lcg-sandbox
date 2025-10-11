1e26154ea5ed0e0fbc0edf8ad203ecf2f4f9880f
martinaspeciale <mspeciale127001@gmail.com>
2025-10-11 12:06:36 +0200

    Make ex1_hello_langchain interactive by allowing user input for topic

--- exercises/ex1_hello_langchain.py
+++ exercises/ex1_hello_langchain.py
@@ -2,13 +2,15 @@ from langchain_core.prompts import ChatPromptTemplate
 from exercises.llm_provider import get_chat_llm
 
 def main():
+    topic = input("\n\n Input a topic to be explained: ")
     llm = get_chat_llm()
     prompt = ChatPromptTemplate.from_messages([
         ("system", "You are a concise tutor."),
         ("human", "Explain {topic} in one paragraph for a beginner.")
     ])
     chain = prompt | llm
-    print(chain.invoke({"topic": "what an API is"}).content)
+    response = chain.invoke({"topic":topic})
+    print("\n\n 🧠 Answer from the model: ", response.content, "\n\n")
 
 if __name__ == "__main__":
     main()
